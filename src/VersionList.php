1a93fed9fc6a1aac35de3b8183ab47862776ca43
Philip Graham <philip.robert.graham@gmail.com>
2014-07-11 14:36:05 +0000

    [minor] Use tabs for indentation

--- src/VersionList.php
+++ src/VersionList.php
@@ -4,8 +4,8 @@
  * Copyright (c) 2012, Philip Graham
  * All rights reserved.
  *
- * This file is part of dbUp and is licensed by the Copyright holder under the
- * 3-clause BSD License.  The full text of the license can be found in the
+ * This file is part of DbUp and is licensed by the Copyright holder under the
+ * 3-clause BSD License.	The full text of the license can be found in the
  * LICENSE.txt file included in the root directory of this distribution or at
  * the link below.
  * =============================================================================
@@ -14,7 +14,7 @@
  */
 namespace zpt\dbup;
 
-use \Iterator;
+use Iterator;
 
 /**
  * This class encapsulates a list of database versions and the scripts that
@@ -24,39 +24,39 @@ use \Iterator;
  */
 class VersionList implements Iterator {
 
-  private $_versions = array();
+	private $_versions = array();
 
-  public function addScript($type, $version, $path) {
-    if (!isset($this->_versions[$version])) {
-      $this->_versions[$version] = array();
-    }
+	public function addScript($type, $version, $path) {
+		if (!isset($this->_versions[$version])) {
+			$this->_versions[$version] = array();
+		}
 
-    $this->_versions[$version][$type] = $path;
-  }
+		$this->_versions[$version][$type] = $path;
+	}
 
-  /* ------------------------------------------------------------------------
-   * Iterator implementation
-   * ------------------------------------------------------------------------ */
+	/* ------------------------------------------------------------------------
+	 * Iterator implementation
+	 * ------------------------------------------------------------------------ */
 
-  public function current() {
-    return current($this->_versions);
-  }
+	public function current() {
+		return current($this->_versions);
+	}
 
-  public function key() {
-    return key($this->_versions);
-  }
+	public function key() {
+		return key($this->_versions);
+	}
 
-  public function next() {
-    next($this->_versions);
-  }
+	public function next() {
+		next($this->_versions);
+	}
 
-  public function rewind() {
-    reset($this->_versions);
-  }
+	public function rewind() {
+		reset($this->_versions);
+	}
 
-  public function valid() {
-    // The versions array should never contain the value false so this is safe.
-    return current($this->_versions) !== false;
-  }
-   
+	public function valid() {
+		// The versions array should never contain the value false so this is safe.
+		return current($this->_versions) !== false;
+	}
+
 }
